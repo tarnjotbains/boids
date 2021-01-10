21e75786072adf5786d1492993012102e9630570
tarnjotbains <tarnjotbains@gmail.com>
2021-01-10 13:10:48 -0500

    Fixed PyQt Implementation

--- main.py
+++ main.py
@@ -1,66 +1,74 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan  2 09:25:49 2021
-
-@author: tarnjotbains
-
-This is a script that uses pyQtGraph and BoidSim to visualize 100 boids. 
-"""
-
-from pyqtgraph.Qt import QtGui, QtCore
+import PyQt5.QtWidgets as qtw
+from PyQt5 import QtCore
+from pyqtgraph import PlotWidget, plot 
 import pyqtgraph as pg
+import sys 
+import os 
 from boids import BoidSim
 
-#-----------------------------------------------------------------------------
-#                               Config
-#-----------------------------------------------------------------------------
+#----------------------------------------------------------------------------------
+#                                       Config
+#----------------------------------------------------------------------------------
+
+# Number of boids to simulate. 
 NUMBOIDS = 50
-XAXISMIN= -500
+
+# Limits of the axis 
+XAXISMIN = -500
 XAXISMAX = 500
 YAXISMIN = -500
 YAXISMAX = 500
-#-----------------------------------------------------------------------------
 
+# Speed is how often the graph gets updated with new boid positions in Milliseconds
+SPEED = 10 
+#----------------------------------------------------------------------------------
 
-def update():
-    """
-    Updates the plot with new boid positions. 
+class MainWindow(qtw.QWidget): 
+    def __init__ (self, num_boids, x_axis_min, x_axis_max, y_axis_min, y_axis_max, speed): 
+        super().__init__() 
+        self.setWindowTitle('Boids Simulation')
+        self.setLayout(qtw.QVBoxLayout())
 
-    Returns
-    -------
-    None.
+        self.plot_boids(num_boids,x_axis_min, x_axis_max, y_axis_min, y_axis_max)
 
-    """
-    global p1, sim, curve
-    sim.move_all_boids_to_new_positions()
-    curve.setData(x= sim.boids[:,0], y= sim.boids[:,1]) 
+        self.timer= QtCore.QTimer()
+        self.timer.setInterval(speed) 
+        self.timer.timeout.connect(self.update) 
+        self.timer.start()
 
+        self.show()  
 
+    def plot_boids(self,num_boids,x_axis_min, x_axis_max, y_axis_min, y_axis_max): 
+        container = pg.PlotWidget() 
+        container.hideAxis('bottom')
+        container.hideAxis('left')
 
-#QtGui.QApplication.setGraphicsSystem('raster')
-app = QtGui.QApplication([])
-    #mw = QtGui.QMainWindow()
-#   mw.resize(800,800)
+        self.sim = BoidSim(num_boids)
 
-win = pg.GraphicsLayoutWidget(show=True, title="Boids Simulation")
-win.resize(1920,1080)
-win.setWindowTitle('Boids Simulation')
+        container.setRange(xRange=[x_axis_min, x_axis_max])
+        container.setRange(yRange=[y_axis_min, y_axis_max])
 
-    # Enable antialiasing for prettier plots
-pg.setConfigOptions(antialias=True)
-
-p1 = win.addPlot(title="A flock of Boids")
-p1.setRange(xRange=[XAXISMIN,XAXISMAX])
-p1.setRange(yRange=[YAXISMIN ,YAXISMAX])
-
-sim = BoidSim(NUMBOIDS) 
-
-curve = p1.plot(x= sim.boids[:,0], y= sim.boids[:,1], pen=None, symbol='t', 
+        self.plot= container.plot(x= self.sim.boids[:,0], y= self.sim.boids[:,1], pen=None, symbol='t', 
                 symbolPen=None, symbolSize=20, symbolBrush=(195,46,212))
-    
-timer = QtCore.QTimer()
-timer.timeout.connect(update) 
-timer.start(10)
+        
+        self.layout().addWidget(container) 
+
+    def update(self):
+        """
+        Updates the plot with new boid positions. 
+
+        Returns
+        -------
+        None.
+
+        """
+        self.sim.move_all_boids_to_new_positions()
+        self.plot.setData(x= self.sim.boids[:,0], y= self.sim.boids[:,1]) 
+
+
+if __name__ == "__main__":
+    app = qtw.QApplication([]) #keeps track of things under the hood. 
+    mw = MainWindow(NUMBOIDS, XAXISMIN, XAXISMAX, YAXISMIN, YAXISMAX, SPEED) 
+    app.exec_() 
     
             
